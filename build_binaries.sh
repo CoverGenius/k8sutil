dceaf11b9ca81efca7d496fe36c0c33517593fa1
Amit Saha <amitsaha.in@gmail.com>
2020-05-07 17:08:06 +1000

    Fix binary name

--- build_binaries.sh
+++ build_binaries.sh
@@ -8,7 +8,7 @@ export GO111MODULE=on
 for pair in linux/amd64 darwin/amd64 windows/amd64; do
     GOOS=`echo $pair | cut -d'/' -f1`
     GOARCH=`echo $pair | cut -d'/' -f2`
-    OBJECT_FILE="xops-$VERSION-$GOOS-$GOARCH"
+    OBJECT_FILE="k8sutil-$VERSION-$GOOS-$GOARCH"
     GOOS=$GOOS GOARCH=$GOARCH go build -o "$DISTDIR/$OBJECT_FILE"
 done
 
